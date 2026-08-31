98cd63b2958695857d8ac7bde684d9526f297a97
Talhah Zahid <talhazahid415@gmail.com>
2026-08-31 18:48:38 +0500

    solve reverse string with two pointer approach

---
+++ array&string/index.js
@@ -0,0 +1,16 @@
+// reverse string using two pointes approach
+
+let str = "hello";
+let strArr = str.split("");
+
+let start = 0;
+let end = strArr.length - 1;
+
+while (start < end) {
+  let temp = strArr[start];
+  strArr[start] = strArr[end];
+  strArr[end] = temp;
+  start++;
+  end--;
+}
+console.log(strArr.join(""));
