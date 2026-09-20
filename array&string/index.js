85733baf46fe2ba45c44d0b6d4e7ac6d6c1b5b48
talhahzahid <talhahzahiddev17@gmail.com>
2026-09-20 07:00:13 +0500

    solved prefix problem

--- array&string/index.js
+++ array&string/index.js
@@ -186,6 +186,9 @@ prefix[0] = arr1[0];
 function buildPrefixSum (arr) {
   let prefix = new Array (arr.length);
   prefix[0] = arr[0];
+  for(let i = 1 ; i < arr.length ; i++){
+    prefix[i] = prefix[i - 1] + arr[i]
+  }
   return prefix;
 } 
 
